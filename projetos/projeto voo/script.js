a2ff28f5348fc9b68c2c33e1a34cd7926a9b5a02
Pedro-Alberto <pedroalbertodias5@gmail.com>
2022-02-27 17:45:13 +0000

    Sistema Avionario

---
+++ projetos/projeto voo/script.js
@@ -0,0 +1,4 @@
+const eldestino = document.getElementById('destino')
+const elorigem = document.getElementById('origem')
+const elnrovoo = document.getElementById('numerovoo')
+const ellugaresdisponiveis = document.getElementById('lugaresdisponiveis')
